e95855335ed9046af5eacca8324944cf8dd8b958
Aelittaaa <144325049+Aelitaaaa@users.noreply.github.com>
2024-08-23 08:57:06 +0700

    update logout

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,26 +1,24 @@
-
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-         <!-- Topbar Navbar -->
-         <ul class="navbar-nav ml-auto">
+    <!-- Topbar Navbar -->
+    <ul class="navbar-nav ml-auto">
 
-
-            <!-- Nav Item - User Information -->
-            <li class="nav-item no-arrow">
-                <a class="nav-link" href="profile.php" role="button" aria-haspopup="true" aria-expanded="false">
+        <!-- Nav Item - User Information -->
+        <li class="nav-item no-arrow">
+            <a class="nav-link" href="profile.php" role="button" aria-haspopup="true" aria-expanded="false">
                 <img class="img-profile rounded-circle" src="assets/img/img_properties/profile.png">
-                </a>
-            </li>
+            </a>
+        </li>
 
         <div class="topbar-divider d-none d-sm-block"></div>
 
         <!-- Nav Item - Logout -->
         <li class="nav-item no-arrow mx-1">
-            <a class="nav-link" href="logout.php" role="button" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-sign-out-alt fa-fw"></i>
-            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="nav-link btn btn-link" type="submit" role="button" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-sign-out-alt fa-fw"></i> Logout
+                </button>
+            </form>
         </li>
-
-
-        </ul>
-
-      </nav>
+    </ul>
+</nav>
