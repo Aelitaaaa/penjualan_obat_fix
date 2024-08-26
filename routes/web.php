de168c6c5c3a5a5b31db91be106e83a691d1f403
Aelittaaa <144325049+Aelitaaaa@users.noreply.github.com>
2024-08-26 10:09:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\ObatController; // Pastikan controller diimpor
 |
 */
 
-// Route untuk login
+
 Route::get('/', function () {
     return view('login');
 });
@@ -23,36 +23,36 @@ Route::get('/login', function () {
 })->name('login');
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
 
-// Route untuk index
+
 Route::get('/index', function () {
     return view('index');
 })->name('index');
 
-// Route untuk obat menggunakan controller
+
 Route::get('/obat', [ObatController::class, 'index'])->name('obat');
 
-// Route untuk pasien
+
 Route::get('/pasien', function () {
     return view('pasien');
 })->name('pasien');
 
-// Route untuk suplier
+
 Route::get('/suplier', function () {
     return view('suplier');
 })->name('suplier');
 
-// Route untuk opname
+
 Route::get('/opname', function () {
     return view('opname');
 })->name('opname');
 
-// Route untuk register
+
 use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 
-// Route untuk logout
+
 use App\Http\Controllers\AuthController;
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
