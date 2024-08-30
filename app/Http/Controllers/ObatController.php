c38bcaf9918c802ac8ab94700a8d2d4b69140bc2
Aelittaaa <144325049+Aelitaaaa@users.noreply.github.com>
2024-08-30 08:47:11 +0700

    ulang

--- app/Http/Controllers/ObatController.php
+++ app/Http/Controllers/ObatController.php
@@ -11,39 +11,7 @@ class ObatController extends Controller
     public function index()
     {
         $obat = Obat::all(); 
-        return view('obat.index', compact('obat')); 
+        return view('obat.index', compact('obat'));
     }
-    public function update(Request $request, $id)
-    {
-        // Validasi data
-        $request->validate([
-            'kode_suplier' => 'required',
-            'kode_obat' => 'required',
-            'nama_obat' => 'required',
-            'harga_obat' => 'required|numeric',
-            'jumlah_obat' => 'required|numeric',
-            'Satuan' => 'required',
-            'total_harga_obat' => 'required|numeric',
-        ]);
-    
-      
-        $obat = Obat::findOrFail($id);
-    
-       
-        $obat->kode_suplier = $request->kode_suplier;
-        $obat->kode_obat = $request->kode_obat;
-        $obat->nama_obat = $request->nama_obat;
-        $obat->harga_obat = $request->harga_obat;
-        $obat->jumlah_obat = $request->jumlah_obat;
-        $obat->Satuan = $request->Satuan;
-        $obat->total_harga_obat = $request->total_harga_obat;
-    
-      
-        $obat->save();
-    
-       
-        return redirect()->route('obat.index')->with('success', 'Data Obat berhasil diupdate');
-    }
-    
-    
+
 }
