71dd6d3043665c1182201f1094812dc29b8abb55
Aelittaaa <144325049+Aelitaaaa@users.noreply.github.com>
2024-09-04 08:07:56 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PembelianController;
 
-// Halaman depan/login
+
 Route::get('/', function () {
     return view('login'); 
 })->name('login');
@@ -22,25 +22,23 @@ Route::post('/login', [LoginController::class, 'login']);
 
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
-
-// Logout 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Rute dengan autentikasi
+
 Route::middleware(['auth'])->group(function () {
     // Dashboard
     Route::get('/index', function () {
         return view('index'); 
     })->name('index');
 
-    // CRUD untuk Obat
+    
     Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
     Route::get('/obat/create', [ObatController::class, 'create'])->name('obat.create');
     Route::post('/obat', [ObatController::class, 'store'])->name('obat.store');
     Route::put('/obat/{id}', [ObatController::class, 'update'])->name('obat.update');
     Route::delete('/obat/{id}', [ObatController::class, 'destroy'])->name('obat.delete');
     
-    // CRUD untuk Pembelian
+    
     Route::prefix('pembelian')->name('pembelian.')->group(function () {
         Route::get('/', [PembelianController::class, 'index'])->name('index');
         Route::get('/create', [PembelianController::class, 'create'])->name('create');
@@ -50,7 +48,7 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}', [PembelianController::class, 'destroy'])->name('destroy');
     });
 
-    // Rute lain
+    
     Route::get('/suplier', [SuplierController::class, 'index'])->name('suplier.index');
     Route::get('/pasien', [PasienController::class, 'index'])->name('pasien.index');
     Route::get('/opname', [StockOpnameController::class, 'index'])->name('opname');
