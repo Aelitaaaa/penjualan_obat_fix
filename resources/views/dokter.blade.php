e7ec9278e5c6009536081e4038d19403d5386acb
LAPTOP-BMMD90MP\ASUS <nindiiartkaa@gmail.com>
2024-09-06 11:33:37 +0700

    dokter

--- resources/views/dokter.blade.php
+++ resources/views/dokter.blade.php
@@ -10,15 +10,6 @@
     <!-- Include CSS and JS -->
     @include('template.head')
 
-    <!-- Custom fonts for this template -->
-    <link href="{{asset('template/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{asset('template/css/sb-admin-2.css')}}" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="{{asset('template/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -36,28 +27,11 @@
             <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                
 
                     <!-- Topbar Navbar -->
                     @include('template.navbar')
-                </nav>
+                
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
@@ -288,5 +262,5 @@
     <script src="{{asset('template/js/demo/datatables-edit.js')}}"></script>
 
 </body>
-
+@include('template.script')
 </html>
