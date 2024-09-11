71d0e42f1797f5e7ba9daf0b99b973d2a98130e1
AimHubs <Azrielhubs@gmail.com>
2024-09-11 09:29:45 +0700

    update rekam medis

--- resources/views/rekammedis.blade.php
+++ resources/views/rekammedis.blade.php
@@ -8,18 +8,11 @@
     
     <!-- Include CSS and JS -->
     @include('template.head')
-
-    <!-- Custom fonts for this template -->
-    <link href="{{asset('template/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{asset('template/css/sb-admin-2.css')}}" rel="stylesheet">
-
     @include('template.script')
-    
+
     <!-- Custom styles for this page -->
     <link href="{{asset('template/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
@@ -135,17 +128,13 @@
                         <div class="form-group">
                             <label for="namaPasien">Pasien</label>
                             <select class="form-control" id="namaDokter">
-                                @foreach($pasien as $p)
-                                <option value = "{{$p -> id_pasien}}">{{$p -> nama_pasien}}</option>
-                                @endforeach
+                                
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="namaDokter">Dokter</label>
                             <select class="form-control" id="namaDokter">
-                                @foreach($dokter as $d)
-                                <option value = "{{$d -> id}}">{{$d -> nama}}</option>
-                                @endforeach
+                                
                             </select>
                         </div>
                         <div class="form-group">
@@ -180,17 +169,14 @@
                         <div class="form-group">
                             <label for="editNamaPasien">Pasien</label>
                             <select class="form-control" id="namaDokter">
-                                @foreach($pasien as $p)
-                                <option value = "{{$p -> id_pasien}}">{{$p -> nama_pasien}}</option>
-                                @endforeach
+                                
+                               
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="editNamaDokter">Dokter</label>
                             <select class="form-control" id="namaDokter">
-                                @foreach($dokter as $d)
-                                <option value = "{{$d -> id}}">{{$d -> nama}}</option>
-                                @endforeach
+                                
                             </select>
                         </div>
                         <div class="form-group">
@@ -229,22 +215,7 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="{{asset('template/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
-    <!-- Core plugin JavaScript -->
-    <script src="{{asset('template/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-    <!-- Custom scripts for all pages -->
-    <script src="{{asset('template/js/sb-admin-2.min.js')}}"></script>
-
-    <!-- Page level plugins -->
-    <script src="{{asset('template/vendor/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('template/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="{{asset('template/js/demo/datatables-demo.js')}}"></script>
 
 </body>
 </html>
