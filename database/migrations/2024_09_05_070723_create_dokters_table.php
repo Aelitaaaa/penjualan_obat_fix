43110dfc359304027bd5ea57313fbefd0a6561d3
AimHubs <Azrielhubs@gmail.com>
2024-10-09 08:32:02 +0700

    cmn benerkan database

--- database/migrations/2024_09_05_070723_create_dokters_table.php
+++ database/migrations/2024_09_05_070723_create_dokters_table.php
@@ -16,6 +16,7 @@ class CreateDoktersTable extends Migration
         Schema::create('dokters', function (Blueprint $table) {
             $table->id();
             $table->string('nama', 100);
+            $table->enum('jenis', ['Umum', 'Spesialis'])->nullable()->default(['Umum', 'Spesialis']);
             $table->string('spesialis', 50);
             $table->string('telp', 15)->unique();
             $table->unsignedBigInteger('tarif');
