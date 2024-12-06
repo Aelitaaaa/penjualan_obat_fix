f3b19b067631c97018487fc2d187283d954475c8
krtn1 <kurniadarman7@gmail.com>
2024-12-06 14:19:52 +0700

    testing app terbaru

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -65,9 +65,9 @@
                   </form>
                   <br>
                   <hr>
-                  <div class="text-center">
+                  {{-- <div class="text-center">
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
+                  </div> --}}
                   <div class="text-center">
                   <a class="small" href="{{ route('register') }}">Buat Akun</a>
                   </div>
